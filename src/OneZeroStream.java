fb6b6ebeb3e29b420ffe3dfd96696a3b5405633f
mistery <sergeorenakan@gmail.com>
2021-09-21 13:17:25 +0300

    third commit

--- src/OneZeroStream.java
+++ src/OneZeroStream.java
@@ -2,9 +2,8 @@ import java.io.InputStream;
 
 public class OneZeroStream extends InputStream {
     private byte[] b = {'1', ' ', '0', ' '};
-    int y;
     int i = 0;
-    static int u;
+    String g;
     public int read() {
         if(i >= b.length)
             i = 0;
